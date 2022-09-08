a723bec8b0d3f0af2aa15df7dd49c0d0e560a193
Johannes Bechberger <johannes.bechberger@sap.com>
2022-09-08 09:15:10 +0200

    Try to improve publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,10 +91,17 @@ publishing {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/parttimenerd/jfrtofp")
+            description = project.description
+            version = project.version
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
         }
     }
 }
